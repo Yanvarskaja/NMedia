d41048ac8cec9a273d76d72ca32da52a7226a56e
Viktotija Popovskaja <yanvarskaja89@gmail.com>
2021-10-03 14:06:23 +0800

    Исправлена ошибка прозрачного фона кнопок

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.likes).setOnClickListener {
+            print("Liked!")
+            (it as ImageButton).setImageResource(R.drawable.ic_baseline_favorite_24)
+        }
     }
 }
