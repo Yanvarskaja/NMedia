383e8293f69eed07b0dc47cdfdacccaea3f068ae
Viktotija Popovskaja <yanvarskaja89@gmail.com>
2021-10-09 13:41:55 +0800

    Добавлена реализация увеличения кол-ва лайков и share

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,16 +1,68 @@
 package ru.netology.nmedia
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.dto.view
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<ImageButton>(R.id.likes).setOnClickListener {
-            print("Liked!")
-            (it as ImageButton).setImageResource(R.drawable.ic_baseline_favorite_24)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val post = Post(
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            author = "Нетология. Университет интернет-профессий будущего"
+        )
+        with(binding) {
+            avatar.setImageResource(R.drawable.ic_netology_48dp)
+            content.text = post.content
+            author.text = post.author
+            published.text = post.published
+            numberLikes.text = view(post.numberLikes)
+
+            likes.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                likes.setImageResource(
+                    if (post.likedByMe) {
+                        R.drawable.ic_baseline_favorite_24
+                    } else {
+                        R.drawable.ic_baseline_favorite_border_24
+                    }
+                )
+                if (post.likedByMe) {
+                    post.numberLikes++
+                    numberLikes.text = view(post.numberLikes)
+                } else {
+                    post.numberLikes--
+                    numberLikes.text = view(post.numberLikes)
+                }
+
+            }
+
+            share.setOnClickListener {
+                post.share = !post.share
+                R.drawable.ic_baseline_share_24
+                if (post.share) {
+                    post.numberShare++
+                    numberShares.text = view(post.numberShare)
+
+
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,27 @@
+package ru.netology.nmedia.dto
+
+data class Post (
+    val id: Long = 0L,
+    val content: String = "",
+    val published: String = "",
+    val author: String = "",
+    var likedByMe: Boolean = false,
+    var numberLikes: Long = 9999L,
+    var share: Boolean = false,
+    var numberShare: Long = 85L,
+    var view:Boolean = false,
+    var numberViews: Long = 1000
+
+        )
+
+
+
+fun view (count : Long) : String {
+        when {
+            count in 1000..9999 -> return (count/1000).toString() + "." + (count % 1000 / 100).toString() +"K"
+            count in 10_000..999_999 -> return (count/1000).toString() + "K"
+           count in 1_000_000..999_999_999 -> return (count/1_000_000).toString() + "M"
+            else -> return count.toString()
+        }
+        return count.toString()
+    }
